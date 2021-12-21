8e6a41d13979b4495009e2fbb1d95dd0421e7f6e
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-12-20 21:52:30 -0800

    Can now lsit categories from the database at /categories path

--- src/categories/categories.controller.js
+++ src/categories/categories.controller.js
@@ -1,1 +1,13 @@
+const categoriesService = require("./categories.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+const list = async (req, res, next) => {
+  try {
+    const data = await categoriesService.list();
+    res.json({ data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { list: asyncErrorBoundary(list) };
