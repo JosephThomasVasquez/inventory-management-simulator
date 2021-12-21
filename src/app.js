55a8705fb964dce052639a2b21b08367d7a31e78
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-12-20 21:37:00 -0800

    Added methodNotAllowed middleware and categories router setup

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
+// Import routes
+const categoriesRouter = require("./categories/categories.router");
+
 const app = express();
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+
+app.use("/categories", categoriesRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "You are connected!" });
--- src/home/home.router.js
+++ src/home/home.router.js
@@ -1,5 +1,3 @@
 const router = require("express").Router();
 
-const router = express.Router();
-
 module.exports = router;
