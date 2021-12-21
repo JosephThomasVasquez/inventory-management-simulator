7f81bf00fc4f653a0f871e812109cca95413a69e
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-12-20 20:53:14 -0800

    Created migrations for categories table

--- src/db/migrations/20211221043701_createCategoriesTable.js
+++ src/db/migrations/20211221043701_createCategoriesTable.js
@@ -1,3 +1,12 @@
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable("categories", (table) => {
+    table.increments("id").primary().unique();
+    table.string("name");
+    table.string("description");
+    table.timestamps(true, true);
+  });
+};
 
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema.dropTable("categories");
+};
