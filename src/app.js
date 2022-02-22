e2c660f98ee0f9ab6323fdbcc4220e242c9c1c36
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2022-02-21 19:27:29 -0800

    Updated cors to use heroku deploy link

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://inventory-manager-backend.herokuapp.com",
     methods: "GET, POST, PUT, DELETE",
     credentials: true,
   })
